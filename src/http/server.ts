e28909e1a58abcab509e702a664a908792fb0c22
Pedro Antunes <pedroabernardes11@gmail.com>
2024-10-10 13:03:35 -0300

    chore: inital commit

---
+++ src/http/server.ts
@@ -0,0 +1,24 @@
+import fastifyCors from '@fastify/cors'
+import fastify from 'fastify'
+import {
+  validatorCompiler,
+  serializerCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.register(fastifyCors, {
+  origin: '*',
+})
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP Server Running!')
+  })
