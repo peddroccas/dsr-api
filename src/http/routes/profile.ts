e4f3e3d3069f64be5a52a545afb425f7e3a119bc
Pedro Antunes <pedroabernardes11@gmail.com>
2024-10-22 13:19:59 -0300

    feat: add verify to profile

--- src/http/routes/profile.ts
+++ src/http/routes/profile.ts
@@ -1,13 +1,20 @@
 import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
 
 import { getProfile } from '../../use-cases/user/get-profile'
+import { verifyJWT } from '../middlewares/verify-jwt'
 
 export const profileRoute: FastifyPluginAsyncZod = async app => {
-  app.post('/profile', async (request, reply) => {
-    const id = request.user.sub
+  app.get(
+    '/profile',
+    {
+      onRequest: [verifyJWT],
+    },
+    async (request, reply) => {
+      const id = request.user.sub
 
-    const { profile } = await getProfile({ id })
+      const { profile } = await getProfile({ id })
 
-    reply.status(201).send({ profile })
-  })
+      reply.status(201).send({ profile })
+    }
+  )
 }
