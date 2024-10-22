508dbb51073d9dc4e6b74da8292994e216299f71
Pedro Bernardes <pedroabernardes11@gmail.com>
2024-10-22 20:43:26 -0300

    feat: first admin route

--- src/http/controllers/user.ts
+++ src/http/controllers/user.ts
@@ -6,6 +6,7 @@ import { updateUserRoute } from '../routes/update-user'
 import { deleteUserRoute } from '../routes/delete-user'
 import { getManagersRoute } from '../routes/get-managers'
 import { profileRoute } from '../routes/profile'
+import { createFirstAdminRoute } from '../routes/create-first-admin'
 
 export async function userRoutes(app: FastifyInstance) {
   app.register(loginRoute)
@@ -15,4 +16,5 @@ export async function userRoutes(app: FastifyInstance) {
   app.register(deleteUserRoute)
   app.register(getManagersRoute)
   app.register(profileRoute)
+  app.register(createFirstAdminRoute)
 }
--- src/http/routes/create-first-admin.ts
+++ src/http/routes/create-first-admin.ts
@@ -1,14 +1,11 @@
 import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
 import { z } from 'zod'
 import { createNewUser } from '../../use-cases/user/create-new-user'
-import { verifyJWT } from '../middlewares/verify-jwt'
-import { verifyAdmin } from '../middlewares/verify-admin'
 
-export const createNewUserRoute: FastifyPluginAsyncZod = async app => {
+export const createFirstAdminRoute: FastifyPluginAsyncZod = async app => {
   app.post(
-    '/users',
+    '/first-admin',
     {
-      onRequest: [verifyJWT, verifyAdmin],
       schema: {
         body: z.object({
           name: z.string(),
