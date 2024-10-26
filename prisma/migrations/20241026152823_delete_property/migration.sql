1561f504496a10383ca2c49dc9e754ef080b1581
Pedro <pedroabernardes11@gmail.com>
2024-10-26 13:14:59 -0300

    refactor: get managers reply

---
+++ prisma/migrations/20241026152823_delete_property/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `position` on the `managers` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "managers" DROP COLUMN "position";
--- src/use-cases/user/get-managers.ts
+++ src/use-cases/user/get-managers.ts
@@ -1,14 +1,36 @@
-import type { User } from '@prisma/client'
 import { prisma } from '../../lib/prisma'
 
 interface getManagersResponse {
-  managers: User[]
+  managers: {
+    name: string
+    store: string
+    id: string
+    email: string
+  }[]
 }
 
 export async function getManagers(): Promise<getManagersResponse> {
-  const managers = await prisma.user.findMany({
-    where: { role: 'MANAGER' },
-  })
+  const managers = await prisma.user
+    .findMany({
+      where: { role: 'MANAGER' },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        manager: { select: { store: { select: { name: true } } } },
+      },
+    })
+    .then(managers =>
+      managers.map(managerResponse => {
+        const manager = {
+          id: managerResponse.id,
+          name: managerResponse.name,
+          email: managerResponse.email,
+          store: managerResponse.manager!.store.name,
+        }
+        return manager
+      })
+    )
 
   return { managers }
 }
