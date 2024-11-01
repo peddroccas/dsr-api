9d94b3cb32cd30b38fd101c1e10542bde38d8558
Pedro Antunes <pedroabernardes11@gmail.com>
2024-11-01 16:33:51 -0300

    fix: register error

--- src/http/controllers/completion.ts
+++ src/http/controllers/completion.ts
@@ -3,10 +3,12 @@ import { createNewCompletionRoute } from '../routes/create-new-completion'
 import { getPendingCompletionsByManagerRoute } from '../routes/get-pending-completions'
 import { approveCompletionRoute } from '../routes/approve-completion'
 import { denieCompletionRoute } from '../routes/denie-completion'
+import { getApprovedCompletionsByManagerRoute } from '../routes/get-approved-completions'
 
 export async function completionRoutes(app: FastifyInstance) {
   app.register(createNewCompletionRoute)
   app.register(getPendingCompletionsByManagerRoute)
+  app.register(getApprovedCompletionsByManagerRoute)
   app.register(approveCompletionRoute)
   app.register(denieCompletionRoute)
 }
